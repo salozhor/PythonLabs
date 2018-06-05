862ab91678b8a8b9eafde00a612964cf34115b70
salozhor <berdnikov_ev1_16@student.grsu.by>
2018-06-05 12:01:34 +0300

    123

--- Графика.py
+++ Графика.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pylab import setp
 def func1(x):
-	return 5+0.5*x
+	return (4*x-12)/3
 	
 def func2(x):
-	return 16
+	return -8+4*x
 	
 def func3(x):
-	return 2*x-1
+	return 8-4*x
 	
 def func4(x):
 	return 0
@@ -22,12 +22,12 @@ def plotit(start, end, n):
 	y2 = np.array([func2(item) for item in x])
 	y3 = np.array([func3(item) for item in x])
 	y4 = np.array([func4(item) for item in x])
-	y5 = np.array([func5(item) for item in x])
-	plt.plot(x, y1, label = "x1 - 2x2 <= -10")
-	plt.plot(x, y2, label = "x2 <= 16")
-	plt.plot(x, y3, label = "2x1 - x2 <= 1")
-	plt.plot(x, y4, label = "x2 >= 0")
-	plt.plot(x, y5, label = "4x1 - 2x2 = 2")
+	#y5 = np.array([func5(item) for item in x])
+	plt.plot(x, y1, label = "4x1-3x2 >= 12")
+	plt.plot(x, y2, label = "-4x1+x2 >= -8")
+	plt.plot(x, y3, label = "4x1+x2 <= 8")
+	plt.plot(x, y4, label = "x2 <= 0")
+	#plt.plot(x, y5, label = "4x1 - 2x2 = 2")
 	#line0 = plt.plot(x2, y2)
 	#setp(line0, linewidth=1, color='y')
 	
